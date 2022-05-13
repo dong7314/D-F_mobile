9afb24b977de3445f39c4951149dcf78d75ffbcc
DongYeopLee <103983410+dong7314@users.noreply.github.com>
2022-05-14 00:11:02 +0900

    lgnb css add
    lgnb css add

--- js/common.js
+++ js/common.js
@@ -10,45 +10,58 @@ $(function(){
     }
   });
 
+  // lgnb
+  $('a.bar-left').click(function(){
+    $('#lgnb').addClass('on');
+    $('#lgnb-wrapper').addClass('on');
+    $('html').css('overflow', 'hidden');
+    return false;
+  });
+  $('.lgnb-top').click(function(){
+    $('#lgnb').removeClass('on');
+    $('#lgnb-wrapper').removeClass('on');
+    $('html').css('overflow', 'auto');
+  });
+
   // messege-btn
   $('.messege-btn').click(function(){
     if ( $(this).hasClass('changed') ) {
       $(this).animate({
         marginLeft: 0,
         width: 80,
-      }, 400);
-      $('#sidebar').animate({marginLeft: -220}, 400);
+      }, 200);
+      $('#sidebar').animate({marginLeft: -220}, 200);
       $('.messege-btn span:nth-of-type(1)').animate({
         marginLeft: 55,
         marginTop: 17,
         width: 8,
         height: 2,
-      }, 400);
+      }, 200);
       $('.messege-btn span:nth-of-type(2)').animate({
         marginLeft: 55,
         marginTop: 3,
         width: 8,
         height: 2,
-      }, 400);
+      }, 200);
       $('.messege-btn span').css('background', '#b71c08');
     } else {
       $(this).animate({
         marginLeft: 220,
         width: 40,
-      }, 400);
-      $('#sidebar').animate({marginLeft: 0}, 400);
+      }, 200);
+      $('#sidebar').animate({marginLeft: 0}, 200);
       $('.messege-btn span:nth-of-type(1)').animate({
         marginLeft: 7.5,
         marginTop: 19,
         width: '60%',
         height: 3,
-      }, 400);
+      }, 200);
       $('.messege-btn span:nth-of-type(2)').animate({
         marginLeft: 7.5,
         marginTop: -3,
         width: '60%',
         height: 3,
-      }, 400);
+      }, 200);
       $('.messege-btn span').css('background', '#b7b7b7');
     }
     $(this).toggleClass('changed');
@@ -151,5 +164,5 @@ $(function(){
     }
   });
 
-  
+
 });
