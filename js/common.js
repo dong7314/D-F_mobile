64c385b5f060b47f32cc9264fc64ba38e0a0a574
DongYeopLee <103983410+dong7314@users.noreply.github.com>
2022-05-13 17:41:02 +0900

    lgnb menu create, animate complete
    lgnb menu create, animate complete

--- js/common.js
+++ js/common.js
@@ -95,7 +95,7 @@ $(function(){
   // animation
   $(window).scroll(function(){
     let winScroll = $(this).scrollTop();
-    $('section').each(function(){
+    $('.part').each(function(){
       let secOffset = $(this).offset().top;
       let secHeight = $(this).height();
       if ( winScroll >= secOffset - 400 && winScroll < secOffset + secHeight ) {
@@ -105,7 +105,15 @@ $(function(){
       }
     });
   });
-  // section0
+
+  // section0, bg-load
+  $('.messege-btn').click();
+  $('#bg-load').addClass('active');
+  setTimeout(function(){
+    $('#section0').addClass('active');
+    $('#bg-load').css('z-index', '0');
+  }, 1000);
+
   // section1 lightning
   let lightTime = setInterval(function(){
     $('#section1 .lightning .lt').each(function(){
@@ -120,6 +128,7 @@ $(function(){
       }, 1000);
     });
   }, 4000);
+
   // section2 character click
   $('.cha2_2').click(function(){
     $('.cha2_2 img:nth-child(1)').css('display', 'block');
@@ -131,4 +140,16 @@ $(function(){
     $('.cha2_2 img:nth-child(1)').css('display', 'none');
     $('.frame').html('<iframe width="565" height="318" src="https://www.youtube.com/embed/-_mDomrcz48" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
   });
+
+  // section2 part active1 class add
+  $(window).scroll(function(){
+    let winScroll = $(this).scrollTop();
+    if ( winScroll >= 2500 && winScroll < 3870 ) {
+      $('#section2 > .part').addClass('active1');
+    } else {
+      $('#section2 > .part').removeClass('active1');
+    }
+  });
+
+  
 });
